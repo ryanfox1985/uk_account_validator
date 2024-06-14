6ba7863b61c4d1262d77978c177f3552c6155029
Guillermo Guerrero <guillermo@guerreroibarra.com>
2024-06-14 14:31:05 +0200

    introduced a method.

--- lib/uk_account_validator/validator.rb
+++ lib/uk_account_validator/validator.rb
@@ -1,22 +1,20 @@
 module UkAccountValidator
   class Validator
 
-    NON_NUMBERS_EXPRESSION = /[^0-9]/
-
     attr_accessor :account_number,
                   :sort_code
 
     def initialize(account_number = nil, sort_code = nil)
-      @account_number = account_number.to_s.gsub(NON_NUMBERS_EXPRESSION, '')
-      @sort_code      = sort_code.to_s.gsub(NON_NUMBERS_EXPRESSION, '')
+      @account_number = remove_non_digits(account_number)
+      @sort_code      = remove_non_digits(sort_code)
     end
 
     def sort_code=(sort_code)
-      @sort_code = sort_code.to_s.gsub(NON_NUMBERS_EXPRESSION, '')
+      @sort_code = remove_non_digits(sort_code)
     end
 
     def account_number=(account_number)
-      @account_number = account_number.to_s.gsub(NON_NUMBERS_EXPRESSION, '')
+      @account_number = remove_non_digits(account_number)
     end
 
     def modulus_weights
@@ -88,6 +86,12 @@ module UkAccountValidator
       else
         BaseException
       end
+    end
+
+    def remove_non_digits(value)
+      return "" if value.nil?
+
+      value.gsub(/[^0-9]/, '')
     end
   end
 end
